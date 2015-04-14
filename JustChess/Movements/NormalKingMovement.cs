6a03c18223796feb2e6a9f1c380d40d7d9414f16
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-04-14 11:26:52 +0300

    Kings movements implemented
    No matter how mighty a king is, he is always under the чехъл of his Queen. Since she is a little bit деспотична and makes the king cry every day, I have decided to help him! Now the king can hide from the Queen all over the gameboard or at least... die trying!
    
    Implemented King's movement:
    - one square in every direction (including diagonals)
    
    !NB: Castling movement is not implemented yet.

---
+++ JustChess/Movements/NormalKingMovement.cs
@@ -0,0 +1,32 @@
+﻿namespace JustChess.Movements
+{
+    using System;
+
+    using JustChess.Board.Contracts;
+    using JustChess.Common;
+    using JustChess.Figures.Contracts;
+    using JustChess.Movements.Contracts;
+
+    public class NormalKingMovement : IMovement
+    {
+        private const string KingInvalidMove = "Kings cannot move this way!";
+
+        public void ValidateMove(IFigure figure, IBoard board, Move move)
+        {
+            var rowDistance = Math.Abs(move.From.Row - move.To.Row);
+            var colDistance = Math.Abs(move.From.Col - move.To.Col);
+            var to = move.To;
+            var figureAtPosition = board.GetFigureAtPosition(to);
+
+            if ((rowDistance == 1 && colDistance == 0) || (rowDistance == 0 && colDistance == 1) || (rowDistance == 1 && colDistance == 1))
+            {
+                if (figureAtPosition == null || figureAtPosition.Color != figure.Color)
+                {
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException(KingInvalidMove);
+        }
+    }
+}
